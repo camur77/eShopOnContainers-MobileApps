652d54dd034f39679df92a08f632635a0ed4ff10
Halil Çamur <91526349+camur77@users.noreply.github.com>
2022-06-10 12:28:12 +0300

    bazı login firebase kodları

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/Helpers/AuthHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace eShopOnContainers.Core.ViewModels.Helpers
+{
+    public interface Iauth
+    {
+        Task<bool> RegisterUser(string name, string email, string password);
+        Task<bool> AuthenticateUser(string email, string password);
+        bool IsAuthenticated();
+        string GetCurrentUserId();
+    }
+
+    public class Auth
+    {
+
+        private static Iauth auth = DependencyService.Get<Iauth>();
+
+        public static async Task<bool> RegisterUser(string name, string email, string password)
+        {
+              return await auth.RegisterUser(name,email,password);
+        }
+
+    public static async Task<bool> AuthenticateUser( string email, string password)
+        {
+              return await auth.AuthenticateUser(email,password);
+        }
+
+        public static bool IsAuthenticated()
+        {
+            return auth.IsAuthenticated();
+        }
+        public static string GetCurrentUserUd()
+        {
+            return auth.GetCurrentUserId();
+        }
+    }
+}
---
+++ eShopOnContainers/eShopOnContainers.Droid/Dependencies/Auth.cs
@@ -0,0 +1,96 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Extensions;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using eShopOnContainers.Core.ViewModels.Helpers;
+using Firebase.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly : Dependency(typeof(Auth)) ]
+namespace eShopOnContainers.Droid.Dependencies
+{
+   public class Auth : Iauth
+    {
+        public Auth()
+        {
+
+        }
+
+        public async Task<bool> AuthenticateUser(string email, string password)
+        {
+            try
+            {
+                await Firebase.Auth.FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+
+               
+                return true;
+            }
+            catch (FirebaseAuthWeakPasswordException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidCredentialsException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidUserException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("try again");
+            }
+        }
+
+        public string GetCurrentUserId()
+        {
+            return Firebase.Auth.FirebaseAuth.Instance.CurrentUser.Uid;
+        }
+
+        public bool IsAuthenticated()
+        {
+            return Firebase.Auth.FirebaseAuth.Instance.CurrentUser != null;
+        }
+
+        public async Task<bool> RegisterUser(string name, string email, string password)
+        {
+
+            try 
+            { 
+            await Firebase.Auth.FirebaseAuth.Instance.CreateUserWithEmailAndPassword(email, password);
+
+            var profileUpdates = new Firebase.Auth.UserProfileChangeRequest.Builder();
+            profileUpdates.SetDisplayName(name);
+            var build = profileUpdates.Build();
+            var user = Firebase.Auth.FirebaseAuth.Instance.CurrentUser;
+            await user.UpdateProfileAsync(build);
+            return true;
+            }
+            catch(FirebaseAuthWeakPasswordException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthInvalidCredentialsException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (FirebaseAuthUserCollisionException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("try again");
+            }
+        }
+    }
+}
