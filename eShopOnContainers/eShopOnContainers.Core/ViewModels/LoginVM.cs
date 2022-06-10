1ad45aa2cb4a07be7ec6357f8965f2f253172abb
Halil Çamur <91526349+camur77@users.noreply.github.com>
2022-06-10 13:43:05 +0300

    eklemeler

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/LoginVM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    public class LoginVM : INotifyPropertyChanged
+    {
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertChanged("Name");
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                OnPropertChanged("Email");
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertChanged("Password");
+            }
+        }
+
+        private string confirmPassword;
+        public string ConfirmPassword
+        {
+            get
+            {
+                return confirmPassword;
+            }
+            set
+            {
+                confirmPassword = value;
+                OnPropertChanged("ConfirmPassword");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public LoginVM()
+        {
+
+        }
+
+        private void OnPropertChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
