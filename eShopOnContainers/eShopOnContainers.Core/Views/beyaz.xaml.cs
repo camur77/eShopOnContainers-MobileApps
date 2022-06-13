1d0998dcb621228fac927433455eff1a31fef946
yavuzbilginoglu <yavuzbilginoglu@gmail.com>
2022-06-13 14:16:14 +0300

    ürün database sayfası

--- eShopOnContainers/eShopOnContainers.Core/Views/beyaz.xaml.cs
+++ eShopOnContainers/eShopOnContainers.Core/Views/beyaz.xaml.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
+using System.Timers;
 using Xamarin.Forms.Xaml;
+using eShopOnContainers.Core.ViewModels.Helpers;
+using Firebase.Database;
+using Firebase.Database.Query;
+using eShopOnContainers.Core.Models;
+using eShopOnContainers.Core.ViewModels;
 
 namespace eShopOnContainers.Core.Views
 {
@@ -14,7 +21,48 @@ namespace eShopOnContainers.Core.Views
     {
         public beyaz()
         {
+            vmProduct = new VMProducts();
+            this.BindingContext = vmProduct;
+            //this.BindingContext = this;
             InitializeComponent();
+        }
+
+        private async void lstProducts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            try
+            {
+                if (lstProducts.SelectedItem != null)
+                {
+                    Products product = (Products)e.SelectedItem;
+                    if (product != null)
+                    {
+                        var display = await DisplayActionSheet(product.productName, "Cancel",
+                        null, new string[] { "Edit", "Delete" });
+                        if (display.Equals("Edit"))
+                        {
+                            vmProduct.setProduct(product);
+                        }
+                        else if (display.Equals("Delete"))
+                        {
+                            vmProduct.setProduct(product);
+                            await vmProduct.trnProducts("DELETE");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            lstProducts.SelectedItem = null;
+        }
+
+        private async void telefonbir(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new telefonbir1());
+        }
+        private async void telefoniki(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new telefoniki2());
         }
         private async void telefonbir(object sender, EventArgs e)
         {
